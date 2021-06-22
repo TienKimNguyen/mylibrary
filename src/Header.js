49b3f5eb8d243ecf4163b9e58ead2914055d9f5a
ktngn <ktngn@yahoo.com>
2021-06-22 13:21:45 -0500

    make a general fram and make the header of the library app

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <i class="fas fa-book-reader fa-2x"></i>
+      <h1>My Library</h1>
+    </header>
+  );
+}
+export default Header;
