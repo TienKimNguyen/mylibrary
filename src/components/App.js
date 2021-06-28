0f8b32d4fac13c32832f7c8b7f920d64bf41b492
ktngn <ktngn@yahoo.com>
2021-06-28 09:31:43 -0500

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, {Component, useState} from "react";
+import React, {Component} from "react";
 import Header from "./Header";
 import PaginationPage from "./PaginationComponent";
 import BookList from "./BookList";
--- src/components/PaginationComponent.js
+++ src/components/PaginationComponent.js
@@ -4,7 +4,7 @@ import React from "react";
 const PaginationPage = (props) => {
     const pageLinks = []
     for (let i = 1; i <= props.pages + 1; i++){
-        let active = props.currentPage === i ? 'active' : '';
+        // let active = props.currentPage === i ? 'active' : '';
         pageLinks.push(<li className="pageNum"  key={i} onClick={() => props.nextPage(i)}>
             <a href="#" className="pageNum">{i}</a>
         </li>)
@@ -20,32 +20,6 @@ const PaginationPage = (props) => {
             
         </div>
     )
-
-    // return (
-    //     <div>
-    //         <Pagination style={{display:"flex", justifyContent:"center"}} >
-    //             {/* <Pagination.First /> */}
-    //             <Pagination.Prev onClick={props.prevPage}/>
-    //             <Pagination.Item disabled>{"Prev"}</Pagination.Item>
-    //             <Pagination.Item disabled>{"..."}</Pagination.Item>
-    //             <Pagination.Item disabled>{"Next"}</Pagination.Item>
-
-
-    //            {/*} <Pagination.Ellipsis />
-
-    //             <Pagination.Item>{10}</Pagination.Item>
-    //             <Pagination.Item>{11}</Pagination.Item>
-    //             <Pagination.Item>{12}</Pagination.Item>
-    //             <Pagination.Item>{13}</Pagination.Item>
-    //             <Pagination.Item>{14}</Pagination.Item>
-                
-    //             <Pagination.Ellipsis />
-    //             <Pagination.Item>{20}</Pagination.Item> */}
-    //             <Pagination.Next onClick={props.nextPage} />
-    //             {/* <Pagination.Last /> */}
-    //         </Pagination>
-    // //   </div>
-    // );
 }
 
 export default PaginationPage;
